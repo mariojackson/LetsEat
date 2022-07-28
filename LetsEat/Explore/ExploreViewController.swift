38e32f5d48aefd7bf2e2c2851ee36c472d91e4f8
Mario Jackson <mario@jackson.softwre>
2022-07-28 21:19:34 +0200

    Added outlet for collection view

---
+++ LetsEat/Explore/ExploreViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreViewController.swift
+//  LetsEat
+//
+//  Created by Mario Jackson on 7/28/22.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+    @IBOutlet var collectionView: UICollectionViewCell!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
